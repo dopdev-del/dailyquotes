18dd71945eba3f19abc3a029f2aea3142b5cc77f
junior-dev <64504551+dopdev-del@users.noreply.github.com>
2024-04-26 03:08:19 +0200

    Update notes.js

--- src/notes.js
+++ src/notes.js
@@ -12,20 +12,6 @@ const notes = [
       title: "GOOGLE PIXEL 8 UNLOCK SERVICE",
       content:
         "“THIS IS A TEMPORARY UNLOCK PROCESS IT WILL WORKS UNTIL YOU FACTORY RESET THE PHONE, SUPPORT UPDATES”"
-    },
-    {
-      key: 3,
-      imgURL: "https://i1.wp.com/www.nuevarevista.net/wp-content/uploads/2019/11/camus-posmo.png?fit=1021%2C580&ssl=1",
-      title: "Albert Camus",
-      content:
-        "“The only serious question in life is whether to kill yourself or not.”"
-    },
-    {
-      key: 4,
-      imgURL: "https://www.filosofia.org/000/platon01.jpg",
-      title: "Platon",
-      content:
-        "“Wise men talk because they have something to say; fools, because they have to say something.”"
     }
   ];
 

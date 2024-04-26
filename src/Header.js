122020bd2402143ae1e1d549b9367385f3262e2d
junior-dev <64504551+dopdev-del@users.noreply.github.com>
2024-04-26 03:11:34 +0200

    Update Header.js

--- src/Header.js
+++ src/Header.js
@@ -3,7 +3,7 @@ import React from "react";
 function Header() {
   return (
     <header>
-      <h1> THE UNLOCK CENTER<p className="lol"><strong> eBay trusted sellers ❤️ we just accept PayPal Payment</strong></p> </h1>
+      <h1> THE UNLOCK CENTER<p className="lol"><strong> eBay trusted sellers we just accept PayPal Payment for direct contact on WhatsApp use: Wa.me/+18297160560</strong></p> </h1>
     </header>
   );
 }
